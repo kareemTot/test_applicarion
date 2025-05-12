21ee5672e9b09e8ed3c3d1774a4ae85b6be99bd3
KareemTot <kareem.mohamed@totplatform.com>
2025-05-12 15:31:20 +0300

    add_packages

--- lib/feature/cart/domain/repo/cart_repo.dart
+++ lib/feature/cart/domain/repo/cart_repo.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:test_applicarion/core/network/errors/failure.dart';
 import 'package:test_applicarion/feature/cart/Data/Model/cart_model/cart_model.dart';
-
 import '../../Data/Model/update_order_dynamic_property_model/update_order_dynamic_property_model.dart';
 
 abstract class CartRepo {
